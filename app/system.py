0b9366912dc90ccfd6aadc474cfaae1e880aecb4
Peter Stakoun <peter@stakoun.com>
2016-02-22 21:45:13 -0500

    Add GPIO

--- app/system.py
+++ app/system.py
@@ -1,10 +1,20 @@
+import RPi.GPIO as GPIO
+
+LED_PIN = 7
+
 running = False
+
+def initGPIO():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LED_PIN, GPIO.OUT)
 
 def start():
     running = True
+    GPIO.output(LED_PIN, True)
 
 def stop():
     running = False
+    GPIO.output(LED_PIN, False)
 
 def isRunning():
     return running
