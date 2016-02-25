13779e7b61cce61eb71a1a07062632b593661468
Peter Stakoun <peter@stakoun.com>
2016-02-25 17:24:50 -0500

    Update pins

--- app/system.py
+++ app/system.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
-ON_LED_PIN = -1
 OFF_LED_PIN = 3
+ON_LED_PIN = 5
 SENSOR_PIN = -1
 CAMERA_PIN = -1
 
@@ -9,22 +9,22 @@ running = False
 
 def initGPIO():
     GPIO.setmode(GPIO.BOARD)
-    #GPIO.setup(ON_LED_PIN, GPIO.OUT)
     GPIO.setup(OFF_LED_PIN, GPIO.OUT)
+    GPIO.setup(ON_LED_PIN, GPIO.OUT)
     #GPIO.setup(SENSOR_PIN, GPIO.IN)
     #GPIO.setup(CAMERA_PIN, GPIO.IN)
-    #GPIO.output(ON_LED_PIN, False)
     GPIO.output(OFF_LED_PIN, True)
+    GPIO.output(ON_LED_PIN, False)
 
 def start():
     running = True
-    #GPIO.output(ON_LED_PIN, True)
     GPIO.output(OFF_LED_PIN, False)
+    GPIO.output(ON_LED_PIN, True)
 
 def stop():
     running = False
     GPIO.output(OFF_LED_PIN, True)
-    #GPIO.output(ON_LED_PIN, False)
+    GPIO.output(ON_LED_PIN, False)
 
 def isRunning():
     return running
