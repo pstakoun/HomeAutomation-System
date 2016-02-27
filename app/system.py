4a7f250e12e6a3eec22522453f426785e624ef68
Peter Stakoun <peter@stakoun.com>
2016-02-27 15:08:25 -0500

    Update GPIO

--- app/system.py
+++ app/system.py
@@ -1,33 +1,27 @@
 import RPi.GPIO as GPIO
 import picamera
 import thread
-import sys
 
 OFF_LED_PIN = 12
 ON_LED_PIN = 11
 SENSOR_PIN = 7
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(OFF_LED_PIN, GPIO.OUT)
+GPIO.setup(ON_LED_PIN, GPIO.OUT)
+GPIO.setup(SENSOR_PIN, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.output(OFF_LED_PIN, True)
+GPIO.output(ON_LED_PIN, False)
+
 running = False
 
 def __init__():
-    global GPIO
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(OFF_LED_PIN, GPIO.OUT)
-    GPIO.setup(ON_LED_PIN, GPIO.OUT)
-    GPIO.setup(SENSOR_PIN, GPIO.IN, GPIO.PUD_DOWN)
-    GPIO.output(OFF_LED_PIN, True)
-    GPIO.output(ON_LED_PIN, False)
-    sys.stdout("a")
     thread.start_new_thread(detectMotion, ())
-    sys.stdout("b")
 
 def detectMotion():
-    sys.stdout("c")
     camera = picamera.PiCamera()
-    sys.stdout("d")
     while True:
-        sys.stdout("e")
-        sys.stdout(GPIO.input(SENSOR_PIN))
+        print(GPIO.input(SENSOR_PIN))
 
 def start():
     global running
