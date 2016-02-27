7638f648a621f004fba45279cb56bbbb1b714abb
Peter Stakoun <peter@stakoun.com>
2016-02-27 10:59:42 -0500

    debug

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ def index():
 @app.route('/start')
 def start():
     system.start()
-    print(str(system.isRunning()), file=sys.stderr)
+    app.logger.info(system.isRunning())
     return str(system.isRunning())
 
 @app.route('/stop')
 def stop():
     system.stop()
-    print(str(system.isRunning()), file=sys.stderr)
+    app.logger.info(system.isRunning())
     return str(system.isRunning())
