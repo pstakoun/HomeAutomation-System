1294ed0748b50ea2480667e522ae0cd16991c4a2
Peter Stakoun <peter@stakoun.com>
2016-03-11 19:47:44 -0500

    Update CORS

--- app/views.py
+++ app/views.py
@@ -20,19 +20,22 @@ def stop():
 def isRunning():
     return str(system.isRunning())
 
-@app.route('/count-captures')
-def countCaptures():
-    return str(system.countCaptures())
-
 @app.route('/captures')
 def captures():
     return str(system.captures)
 
+@app.route('/count-captures')
+@crossdomain(origin='*')
+def countCaptures():
+    return str(system.countCaptures())
+
 @app.route('/capture/<n>')
+@crossdomain(origin='*')
 def capture(n):
     return send_file('/home/pi/HAS-captures/'+system.getCapture(int(n)), mimetype='image/jpeg')
 
 @app.route('/capture/<n>/date')
+@crossdomain(origin='*')
 def captureDate(n):
     fileName = system.getCapture(int(n))
     return fileName[:4]+'-'+fileName[4:6]+'-'+fileName[6:8]+' '+fileName[8:10]+':'+fileName[10:12]+':'+fileName[12:14]
